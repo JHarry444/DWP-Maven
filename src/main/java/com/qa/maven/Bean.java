ff4517adbf51b0970b282bca8a868a5f7da635cd
jordan h <jharrison1@qa.com>
2025-05-21 10:22:22 +0100

    init commit

--- src/main/java/com/qa/maven/Bean.java
+++ src/main/java/com/qa/maven/Bean.java
@@ -1,4 +1,16 @@
 package com.qa.maven;
 
-public class Bean {
+import java.io.Serializable;
+
+public class Bean implements Serializable {
+
+    private String field;
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
 }
--- src/main/java/com/qa/maven/Demo.java
+++ src/main/java/com/qa/maven/Demo.java
@@ -1,4 +1,8 @@
 package com.qa.maven;
 
 public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
